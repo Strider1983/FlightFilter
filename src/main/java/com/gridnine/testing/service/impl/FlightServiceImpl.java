c4aaf7bf4707c88ef14029268c1733594753485b
mezen <mezentsev.k@gmail.com>
2024-07-02 14:39:31 +0800

    java doc created for: FlightServiceImpl

--- src/main/java/com/gridnine/testing/service/impl/FlightServiceImpl.java
+++ src/main/java/com/gridnine/testing/service/impl/FlightServiceImpl.java
@@ -8,6 +8,11 @@ import java.time.LocalDateTime;
 import java.util.Arrays;
 
 public class FlightServiceImpl implements FlightService {
+    /**
+     * gets total flight time in minutes
+     * @param flight
+     * @return long totalFlightMinutes
+     */
     @Override
     public long totalFlightMinutes(Flight flight) {
         LocalDateTime dep = departureTime(flight);
@@ -16,17 +21,35 @@ public class FlightServiceImpl implements FlightService {
         return totalFlightMinutes;
     }
 
+    /**
+     * gets total number of flight transfers
+     * @param flight
+     * @return long totalTransfers
+     */
+
     @Override
     public long totalTransfers(Flight flight) {
         long totalTransfers = flight.getSegments().size() - 1;
         return totalTransfers;
     }
 
+    /**
+     * gets total number of flight segments
+     * @param flight
+     * @return long totalSegments
+     */
+
     @Override
     public long totalSegments(Flight flight) {
         long totalSegments = flight.getSegments().size();
         return totalSegments;
     }
+
+    /**
+     * gets total time on earth between of flight segments in minutes
+     * @param flight
+     * @return long totalEarthTime
+     */
 
     @Override
     public long totalEarthTime(Flight flight) {
@@ -36,6 +59,12 @@ public class FlightServiceImpl implements FlightService {
         }
         return totalEarthTime;
     }
+
+    /**
+     * gets total time in the sky of all flight segments in minutes
+     * @param flight
+     * @return long totalSkyTime
+     */
 
     @Override
     public long totalSkyTime(Flight flight) {
@@ -47,12 +76,23 @@ public class FlightServiceImpl implements FlightService {
         return totalSkyTime;
     }
 
+    /**
+     * gets flight's departure date-time
+     * @param flight
+     * @return LocalDateTime dep
+     */
 
     @Override
     public LocalDateTime departureTime(Flight flight) {
         LocalDateTime dep = flight.getSegments().get(0).getDepartureDate();
         return dep;
     }
+
+    /**
+     * gets flight's arrival date-time
+     * @param flight
+     * @return LocalDateTime arr
+     */
 
     @Override
     public LocalDateTime arrivalTime(Flight flight) {
